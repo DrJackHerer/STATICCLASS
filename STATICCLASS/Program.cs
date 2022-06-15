9844a13eef74f40c4aee9e281117e2d02327b075
drjackherer <mongkolpersonal@gmail.com>
2022-06-15 12:55:46 -0700

    Add project files.

---
+++ STATICCLASS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace STATICCLASS
+{
+     class Program
+    {
+        private static object rng;
+
+        static void Main(string[] args)
+        {
+            //Math MyMath = new Math();
+
+
+            //static class method
+            Math.Pow(5, 2);
+
+            //non static
+            Random rng = new Random();
+
+            rng.Next(0, 100);
+
+            string myName;
+            
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ STATICCLASS/TempConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace STATICCLASS
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(doublefTemp)
+        {
+            var cel
+        }
+    }
+}
